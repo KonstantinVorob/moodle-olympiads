30622f9d6913538c88d0443f16c6276bc3d808db
Konstantin <lera.polshina@yandex.ru>
2025-01-12 01:39:59 +0300

    Создание скелета плагина

---
+++ blocks/olympiads/lang/en/block_olympiads.php
@@ -0,0 +1,3 @@
+<?php
+$string['pluginname'] = 'Olympiads Block';
+$string['blockcontent'] = 'This is the Olympiads block content.';
---
+++ blocks/olympiads/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'block_olympiads';  // Имя компонента.
+$plugin->version = 2025011100;           // Дата и время выпуска в формате ГГГГММДДЧЧММ.
+$plugin->requires = 2021051700;          // Минимальная версия Moodle (3.11).
+$plugin->maturity = MATURITY_ALPHA;      // Стадия разработки (ALPHA, BETA, STABLE).
+$plugin->release = 'v1.0';               // Номер версии плагина.
